62aec047c18b459021831edc2bbfd67c0886b555
peng119 <1774220844@qq.com>
2019-12-31 09:46:08 +0800

    扑克牌

--- 扑克牌/ConsoleApplication1/Properties/AssemblyInfo.cs
+++ 扑克牌/ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("1128")]
+[assembly: AssemblyTitle("ConsoleApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("1128")]
+[assembly: AssemblyProduct("ConsoleApplication1")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("c393e721-52a0-4402-b82e-c692d74f1bd8")]
+[assembly: Guid("cb1dcae1-5bd3-4417-b39d-dc46aa1698a3")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
